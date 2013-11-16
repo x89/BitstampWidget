c15e2225ac1f620d38885b39350266eb139d40ec
David Trail <napalm@trailbeans.eu>
2013-11-16 19:11:14 +0100

    No longer needed, replaced by perl

--- seed.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-while true; do
-	last=`lua ./bitstamp.lua`
-	last=$(printf '%.2f' $last)
-	echo "last_price = '$last'" | awesome-client
-#	echo "Updated to $last"
-	sleep 2
-done
