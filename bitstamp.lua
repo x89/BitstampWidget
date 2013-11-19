5d3756bf1da0a0c3059e94ae03c74281d08bb01a
David Trail <napalm@trailbeans.eu>
2013-11-19 14:08:50 +0100

    Timeout added for when Bitstamp lags due to massive activity

--- bitstamp.lua
+++ bitstamp.lua
@@ -6,6 +6,8 @@ local json = require('dkjson')
 local https = require('ssl.https')
 local posix = require('posix')
 
+local sleep_time = 2
+
 function bitstamp_table()
 	-- Makes an HTTPS request to the bitstamp ticker to get the latest price.
 	-- Keys: low, ask, high, volume, timestamp, last, bid
@@ -14,7 +16,8 @@ function bitstamp_table()
 	assert(https.request{
 		url = "https://www.bitstamp.net:443/api/ticker/",
 		sink = ltn12.sink.table(resp),
-		protocol = "tlsv1"
+		protocol = "tlsv1",
+		timeout = sleep_time
 	}, "Couldn't establish a connection.")
 
 	return json.decode(resp[1])
